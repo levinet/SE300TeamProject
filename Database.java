6dd0ce309790c6398a6168601ece149944c78705
bradlet7 <bradlet7@COB125-17.erau.edu>
2015-10-19 15:11:26 -0400

    Work in Progress

--- Database.java
+++ Database.java
@@ -1,10 +1,11 @@
-//Ibrahim-Push
+import java.io.File;
+import java.io.FileInputStream;
+
 
 /**
  * @author lopezy2
  * @version 1.0
  * @created 09-Oct-2015 3:16:18 PM
- *BS!!!
  *Version 1.0.0
  */
 public class Database {
@@ -12,9 +13,17 @@ public class Database {
 	private double coordinates;
 	private double homeLocation;
 	private double time;
-
+	
 	public Database(){
-
+		
+		//Incomplete; need to get Apache POI
+		
+		File file = new File("P://SEE300ALH.xlsx");
+		FileInputStream fis = new FileInputStream(file);
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		
+		
 	}
 
 	/**m
