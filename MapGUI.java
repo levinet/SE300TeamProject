688c291f79328e322fbe722399f52480c0c28feb
levinet <levinet@my.erau.edu>
2015-10-19 12:55:05 -0400

    1. Google Dynamic Maps... Need 30 day license 2. Google Static Maps...

--- MapGUI.java
+++ MapGUI.java
@@ -5,6 +5,11 @@
  * @version 1.0
  * @created 09-Oct-2015 3:16:18 PM
  */
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserFactory;
+import javax.swing.*;
+import java.awt.*;
+
 public class MapGUI extends LoginGUI {
 
 	private double commonLocation;
@@ -13,10 +18,19 @@ public class MapGUI extends LoginGUI {
 	private char lostinfo;
 	public InfoGUI m_InfoGUI;
 
-	public MapGUI(){
+	public static void main(String[] args) {
+        Browser browser = BrowserFactory.create();
+        JFrame frame = new JFrame("JxBrowser Google Maps");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(browser.getView().getComponent(), BorderLayout.CENTER);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        browser.loadURL("http://maps.google.com");
+    }
 
-	}
 
+	
 	/**
 	 * 
 	 * @exception Throwable
