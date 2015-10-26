a2209db71b2f052f6596b7fccf7354b5c7092d59
Abalkhail <abalkhai@my.erau.edu>
2015-10-26 14:19:12 -0400

    Login GUI with username and password fields. LoginDataBase class is to save login information history for verification of users in case we decide to use that.

--- LoginGUI.java
+++ LoginGUI.java
@@ -1,44 +1,82 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 
-/**
- * @author lopezy2
- * @version 1.0
- * @created 09-October-2015 3:16:18 PM
- */
-public class LoginGUI {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-	public String pasword;
-	public String username;
-	public MapGUI m_MapGUI;
-	
-	public static void main(String[] args){
-		System.out.println("hello world.");
-	}
-	
-	public LoginGUI(){
+public class LoginGUI extends JFrame {
 
-	}
+	private JPanel contentPane;
+	private JPasswordField passwordField;
 
-	public void finalize() throws Throwable {
-
-	}
-	public boolean checkUP(){
-		return false;
-	}
-
-	public String createNewUser(){
-		return "";
-	}
-
-	public String ecLogin(){
-		return "";
-	}
-
-	public String savePassword(){
-		return "";
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginGUI frame = new LoginGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	public String saveUserName(){
-		return "";
+	/**
+	 * Create the frame.
+	 */
+	public LoginGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("User Name");
+		lblNewLabel.setBounds(94, 81, 80, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(94, 115, 80, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setBounds(186, 76, 177, 26);
+		contentPane.add(formattedTextField);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(186, 110, 177, 26);
+		contentPane.add(passwordField);
+		JFrame current = this;
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LoginDataBase loginDb = new LoginDataBase();
+				String user = formattedTextField.getText();
+				String pass = passwordField.getText();
+				
+				if (loginDb.Authenticate(user,pass));
+				{
+					ApplicationWindow AppWin = new ApplicationWindow();
+					AppWin.show();
+					current.dispose();
+				}
+			}
+		});
+		btnLogin.setBounds(186, 154, 117, 29);
+		contentPane.add(btnLogin);
 	}
-}//end LoginGUI
+}
