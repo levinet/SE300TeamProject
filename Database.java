94be1d81a96c2b1db17acb464cb2d0c0c6ed9198
bradlet7 <bradlet7@COB125-17.erau.edu>
2015-10-26 16:12:29 -0400

    Database Push

--- Database.java
+++ Database.java
@@ -1,46 +1,77 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 
-public class Database() {
+public class Database {
 
-	private double coordinates;
-	private double homeLocation;
-	private double time;
-	
-	
-	private class 
-	public static String readFile(){
-	    BufferedReader reader = new BufferedReader( new FileReader ("SE300ALH.xlsx"));
+
+ 	public static String readFile(){
+	    BufferedReader reader = null;
+		try {
+			reader = new BufferedReader( new FileReader ("SE300ALH.xlsx"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	    String         line = null;
 	    StringBuilder  stringBuilder = new StringBuilder();
 	    String         ls = System.getProperty("line.separator");
 
-	    while( ( line = reader.readLine() ) != null ) {
-	        stringBuilder.append( line );
-	        stringBuilder.append( ls );
-	    }
+	    try {
+			while( ( line = reader.readLine() ) != null ) {
+			    stringBuilder.append( line );
+			    stringBuilder.append( ls );
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    
-	    reader.close();
+	    try {
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	    return stringBuilder.toString();
 	}
 	
 	public static void writeToFile(String s){
 		File locFile = new File("SE300ALH.xlsx");
-		locFile.createNewFile();
-		BufferedWriter buffWriter = new BufferedWriter(new FileWriter(locFile, true));
+		try {
+			locFile.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		BufferedWriter buffWriter = null;
+		try {
+			buffWriter = new BufferedWriter(new FileWriter(locFile, true));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
-		buffWriter.write(s);
+		try {
+			buffWriter.write(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		buffWriter.close();
-	}
-	
-	
-}
-	
-		
+		try {
+			buffWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**m
@@ -55,4 +86,5 @@ public class Database() {
 	public void storeData(){
 
 	}
-}//end Database
+}
+//end Database
